d3ae725af3e342e388d8656aafec1bc8aa1e2d9b
vritant24 <vritoku@gmail.com>
2017-10-21 16:50:57 -0400

    added index.js

---
+++ test2/tester2.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+
+describe('Testing problem1: isNumber()', function() {
+    it('Should return true for number', function() {
+      assert.equal(false, true);
+    });
+});
+
+describe('Testing problem2: isNumber()', function() {
+    it('Should return true for number', function() {
+      assert.equal(false, true);
+    });
+});
+
+describe('Testing problem3: isNumber()', function() {
+    it('Should return true for number', function() {
+      assert.equal(true, true);
+    });
+});
